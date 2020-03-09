52a938253facdbf23b215a812f1b303f1eb7f085
Anouar ElGhazaoui <2080049@talnet.nl>
2020-03-09 15:10:51 +0100

    done

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+
+$dsn = 'mysql:host=localhost;dbname=netland';
+$username = 'root';
+$password = '';
+
+try {
+    $conn = new PDO($dsn, $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+?>
